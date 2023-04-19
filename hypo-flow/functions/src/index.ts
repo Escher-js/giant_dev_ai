c011a5046b7c639668ccbb5f720fc9e2c97bf883
psmuler <zhmuler@gmail.com>
2023-04-19 15:18:52 +0900

    firebase functions

---
+++ hypo-flow/functions/src/index.ts
@@ -0,0 +1,9 @@
+// index.ts の場合
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
